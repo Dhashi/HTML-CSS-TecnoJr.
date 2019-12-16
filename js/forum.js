65cb06a4ab13796a9cf21e1367c3c3618393620e
Nicholas <nicholasybiologia@outlook.com>
2019-12-15 22:34:51 -0300

    v4.0
    Mudança da página que seria um tutorial para uma pagina de login do Forum.
    E autenticação de login e senha com JavaScript
    
    ps: Foi criada pasta para arquivos .scss para futura migração de todo o site para esse método de trabalho

--- js/forum.js
+++ js/forum.js
@@ -1,7 +1,14 @@
-function valida_form() {
-    if (document.getElementById("nome").value == "") {
-        alert('Por favor, preencha o campo nome');
+function validacao() {
+    if (document.getElementById("clogin").value == "") {
+        alert('Por favor, preencha o campo "Login"');
         document.getElementById("nome").focus();
+        return false
+    }
+
+    if (document.getElementById("csenha").value == "" ||
+        document.getElementById("csenha").value.length < 5) {
+        alert('A sua senha deve ter pelo menos 5 digitos');
+        document.getElementById("csenha").focus();
         return false
     }
 }
